7b5aa3aafc5aa9e173171cca7cd42ab156fff3b4
Brandon Maul <brandonmaul@users.noreply.github.com>
2025-05-14 19:46:19 -0400

    updating to use brandonmaul

--- Package.swift
+++ Package.swift
@@ -8,6 +8,6 @@ let package = Package(
         .library(name: "MatrixSDK", targets: ["MatrixSDK"])
     ],
     targets: [
-        .binaryTarget(name: "MatrixSDK", url: "https://github.com/Topheee/MatrixSDK/releases/download/v0.27.6/MatrixSDK.xcframework.zip", checksum: "4b10fd35f22785eef88a43832906c1668b9029e50162ebfbeff0c79c907e94ab")
+        .binaryTarget(name: "MatrixSDK", url: "https://github.com/brandonmaul/MatrixSDK/releases/download/v0.27.17/MatrixSDK.xcframework.zip", checksum: "4b10fd35f22785eef88a43832906c1668b9029e50162ebfbeff0c79c907e94ab")
     ]
 )
